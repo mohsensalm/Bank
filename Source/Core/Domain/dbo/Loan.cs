dca5165519689ea858db883e9089baa3862dfbb2
mohsen.sl76 <mohsensalmanzade68@gmail.com>
2024-02-05 23:44:36 +0330

    Add project files.

---
+++ Source/Core/Domain/dbo/Loan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Domain.dbo
+{
+    public class Loan
+    {
+        public virtual int ID { get; set; }
+        public virtual int CoferID { get; set; }
+        public virtual string? Name { get; set; }
+        public virtual int Admin { get; set; }
+        public virtual int TotalPrice { get; set; }
+        public virtual int InstallmentNum { get; set; }
+        public virtual int MemberNum { get; set; }
+        public virtual DateTime StartDate { get; set; }
+        public virtual DateTime EndDate { get; set; }
+        public virtual int Status { get; set; }
+        public virtual string? Description { get; set; }
+        public virtual string? PricePerIns { get; set; }
+
+
+
+
+    }
+}
---
+++ Source/Core/Domain/dbo/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.dbo
+{
+     public class Status
+    {
+        public int ID { get; set; }
+        public int SatusType { get; set; }
+        public string? Title { get; set; }
+    }
+}
